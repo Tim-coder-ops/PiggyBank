b2e7685b731e97133bd07708c8a8b6bdb6b40848
Tim-coder-ops <68295710+Tim-coder-ops@users.noreply.github.com>
2020-08-04 09:35:36 -0700

    Changed total to display dollar amount.

---
+++ PiggyBank/Bank.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiggyBank
+{
+    public class Bank
+    {   //Creating random number and declaring coin array
+        Random randomNumber;
+        static string[] coins = { "penny", "nickel", "dime", "quarter" };
+
+        //Default Constructor
+        public Bank()
+        {
+            randomNumber = new Random();
+        }
+
+        //Single parameter argument
+        public Bank(int seed)
+        {
+            randomNumber = new Random(seed);
+        }
+
+        
+        public int CountmyCoins()
+        {
+            return randomNumber.Next(10, 50);
+        }
+
+        //Returns type of coin based off count
+        public string[] GetCoins(int howMany)
+        {
+            string[] myCoins = new string[howMany];
+
+            for(int i = 0; i < myCoins.Length; i++)
+            {
+                myCoins[i] = coins[randomNumber.Next(0, 4)];
+                Console.Write(myCoins[i] + ", ");
+            }
+            return myCoins;
+        }
+
+        //Calculating the monitary value of all the coins
+        public int HowMuch(string[] AlltheCoins)
+        {
+            int total = 0;
+
+            for(int i = 0; i < AlltheCoins.Length; i++)
+            {
+                if (AlltheCoins[i] == "penny") total++;
+                else if (AlltheCoins[i] == "nickel") total += 5;
+                else if (AlltheCoins[i] == "dime") total += 10;
+                else if (AlltheCoins[i] == "quarter") total += 25;
+            }
+            return total;
+        }
+    }
+}
---
+++ PiggyBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PiggyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Piggy Bank Excercise");
+
+            //Passing a single argument to the constructor 
+            Bank MyBank = new Bank(73);
+
+            //Printing Number of coins in the bank
+            int numCoins = MyBank.CountmyCoins();
+            Console.WriteLine($"There are {numCoins} coins in the bank.");
+
+            //Printing coins in the bank
+            Console.WriteLine($"\nThe coins in the bank are:\n");
+            string[] allMyCoins = MyBank.GetCoins(numCoins);
+
+            //Printing out total 
+            Console.WriteLine($"\n\n There are {MyBank.HowMuch(allMyCoins)} cents in the bank.");
+
+        }
+    }
+}
